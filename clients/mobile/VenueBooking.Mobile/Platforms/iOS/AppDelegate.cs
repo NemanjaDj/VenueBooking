eec4c61b4a267adfdfcd5441fe15d16766873781
NemanjaDj <nemanjadjeric94@gmail.com>
2026-08-25 16:04:12 +0200

    first commit

---
+++ clients/mobile/VenueBooking.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace VenueBooking.Mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ src/Shared/VenueBooking.SharedKernel/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace VenueBooking.SharedKernel;
+
+public class Class1
+{
+
+}
